d7fff73f97d32f2a0a3856715d2138920b670e72
Mitriy_Bug <verstkajoom@gmail.com>
2024-09-04 20:59:26 +0300

    Init

--- src/js/__tests__/validator.test.js
+++
@@ -1,6 +0,0 @@
-import Validator from '../validator';
-test('regExp', () => {
-    const validator = new Validator();
-    const result = validator.validateUsername("rr111tt")
-    expect(result).toBe(true);
-});
