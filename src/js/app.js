19cdfd1f7ebef6ff6db38cd3464539b037dced3a
Mitriy_Bug <verstkajoom@gmail.com>
2024-09-04 20:58:08 +0300

    Init

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+// TODO: write your code here
+//import sum from './basic';
+import Validator from './validator';
+const validator = new Validator();
+console.log(validator.validateUsername("rr111tt"));
